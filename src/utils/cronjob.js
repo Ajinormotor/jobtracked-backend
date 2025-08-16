ae0b36e8c3dc8c0362762eb86f68c31a4307c8fc
Teitei Wisdom <teiteiwisdom@gmail.com>
2025-08-16 16:30:03 +0100

    fix:updated db

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -4,16 +4,12 @@ import https from 'https'
 const job = new cron.CronJob("*/14 * * * *", function () {
     console.log('Sending GET request to:', process.env.API_URL)
 
-https.get(process.env.API_URL, (res) => {
-    if(res.statusCode === 200) console.log('GET request sent successfully')
-        else console.log('GET request failed', res.statusCode);
-}).on("error", (e) => console.error("Error while sending request", e));
-
+    https.get(process.env.API_URL, (res) => {
+        if(res.statusCode === 200) 
+            console.log('GET request sent successfully')
+        else 
+            console.log('GET request failed', res.statusCode);
+    }).on("error", (e) => console.error("Error while sending request", e));
 });
 
-
-
 export default job
-
-
-
