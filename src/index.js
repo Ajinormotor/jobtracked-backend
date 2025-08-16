960a850fc9df527ec9c0fa10fef0dcc2a0f6f0ad
Teitei Wisdom <teiteiwisdom@gmail.com>
2025-08-16 11:14:04 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+import authRoutes from "./auth/auth.route.js"
+import jobRoutes from "./jobs/job.route.js"
+// import job from "./lib/cron.js"
+
+import { connectDB } from "./lib/connectDB.js"
+
+const app = express()
+dotenv.config();
+const PORT = process.env.PORT || 3000
+
+
+job.start();
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("Get request");
+});
+
+app.use('/api/auth', authRoutes)
+app.use('/api/jobs', jobRoutes)
+app.use('/api/users', userRoutes)
+
+
+
+app.listen(PORT, () => {
+        connectDB(),
+      console.log(`✅ Server is listening at http://localhost:${PORT}`)
+    })
+ 
+
+
---
+++ src/jobs/job.controller.js
@@ -0,0 +1,126 @@
+import Job from "../models/jobModels.js";
+
+export const getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({ userId: req.user.id });
+
+    if (jobs.length === 0) {
+      return res.status(404).json({
+        message: 'No jobs found'
+      });
+    }
+
+    // Filter out case-insensitive duplicates
+    const uniqueJobs = jobs.filter((job, index, self) =>
+      index === self.findIndex(j =>
+        j.title.toLowerCase() === job.title.toLowerCase() &&
+        j.company.toLowerCase() === job.company.toLowerCase()
+      )
+    );
+
+    res.status(200).json(uniqueJobs);
+  } catch (error) {
+    // console.error(error.message);
+    res.status(500).json({
+      message: error.message || "Server error while fetching jobs"
+    });
+  }
+};
+
+
+export const createJob = async (req, res) => {
+  try {
+  const { title, company, status, salary, notes, link } = req.body; 
+
+
+const existingJob = await Job.findOne({
+title,
+company,
+userId: req.user.id
+});
+
+if (existingJob) {
+return res.status(400).json({
+message: 'You have already added the job title ${title} position at ${company}.'
+});
+}
+
+
+    const job = await Job.create({
+      title,
+      company,
+      status,
+      salary,
+      notes,
+      link,
+      userId: req.user.id, 
+    });
+
+    res.status(201).json({message: 'Job Created',job});
+  } catch (error) {
+    // console.error('Creating Job Error:', error.message);
+    res.status(500).json({ message: "Server error while creating job" });
+  }
+};
+
+
+export const updateJob = async (req, res) => {
+  try {
+
+      const { id } = req.params;
+    const job = await Job.findOne({ id });
+
+if (!req.body || Object.keys(req.body).length === 0) {
+  return res.status(400).json({
+    message: "Can't send an empty request"
+  });
+}
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+   
+    if (job.userId.toString() !== req.user.id.toString()) {
+  return res.status(401).json({ message: "Not authorized" });
+}
+
+    const updatedJob = await Job.findOneAndUpdate(
+      {id },
+      req.body,
+      { new: true }
+    );
+
+   return  res.status(200).json({ 
+    message: 'Job Updated succesfuly' ,
+     updatedJob});
+  } catch (error) {
+    // console.error("Error updating jobs:",error.message);
+    res.status(500).json({ message: "Server error while updating job" });
+  }
+};
+
+
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    
+    const job = await Job.findOne({ id });
+
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+   
+
+    if (job.userId.toString() !== req.user.id.toString()) {
+  return res.status(401).json({ message: "Not authorized" });
+}
+
+
+    await job.deleteOne();
+
+       return res.status(204).send();
+  } catch (error) {
+    // console.error('Error deleting jobs:',error.message);
+    res.status(500).json({ message: "Server error while deleting job" });
+  }
+};
