cd8cd370ce274e0be0db0394df683df98bb0d2a2
Teitei Wisdom <teiteiwisdom@gmail.com>
2025-08-16 17:29:44 +0100

    fix:updated user details

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,20 +1,21 @@
-export  const getProfile = async(req,res) => {
-    try {
-        const user =req.user.populate('users', 'username', 'email')
-        if(!user) {
-            return res.status(404).json({
-                message: 'User not found'
-            })
-        }
+export const getProfile = async (req, res) => {
+  try {
+    // find the user again from DB
+    const user = await User.findById(req.user.id).select("username email _id createdAt");
 
-        return res.status(200).json({
-            user
-        })
-        
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message || 'Inertnal server errror'
-        })
-        
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
     }
-}
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "Internal server error",
+    });
+  }
+};
