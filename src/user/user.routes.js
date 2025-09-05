71a1c0161041e92e7277db3a986c2ca7d7a96d3e
Teitei Wisdom <teiteiwisdom@gmail.com>
2025-09-05 19:55:52 +0100

    fix:updated user routes

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getProfile,updatePassword } from "./user.controller.js"
+import { getProfile,updatePassword,updateProfile } from "./user.controller.js"
 import protect from "../middleware/auth.middleware.js"
 
 const router = express.Router()
